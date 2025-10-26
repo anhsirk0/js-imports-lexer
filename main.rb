e2af126f04535e40b0080fcca6c695e56917d4a6
krishna <krishna404@yandex.com>
2025-10-26 19:02:14 +0530

    add README

--- main.rb
+++ main.rb
@@ -2,27 +2,28 @@
 
 require_relative "lexer.rb"
 
-src = %{import {
-  Some as Other,
-  imported,
-  Maybe,
-  toMaybe,
-  Stuff,
-  MoreStuff,
-  smallStuff,
-  asTestUtils,
-  froming,
-} from "test";
-// some comments
-import { Some2, Maybe2, Stuff2, MoreStuff2 } from "test2";
-// import { Some3, Maybe3, Stuff3, MoreStuff3 } from "test3";
+# src = %{import {
+#   Some as Other,
+#   imported,
+#   Maybe,
+#   toMaybe,
+#   Stuff,
+#   MoreStuff,
+#   smallStuff,
+#   asTestUtils,
+#   froming,
+# } from "test";
+# // some comments
+# import { Some2, Maybe2, Stuff2, MoreStuff2 } from "test2";
+# // import { Some3, Maybe3, Stuff3, MoreStuff3 } from "test3";
 
-import React, { FC, useState } from "react";
-import * as All from "all";
-import some from "some";
+# import React, { FC, useState } from "react";
+# import * as All from "all";
+# import some from "some";
 
-console.log(123);
-}
+# console.log(123);
+# }
+src = 'import React, { FC , useState } from "react";'
 
 lex = Lexer.new(src)
 lex.parse!
